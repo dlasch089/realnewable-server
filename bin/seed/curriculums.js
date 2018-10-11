cab0b5b5109e08fc215ffa23769056b8b58a7315
jsplmns <jonathan@spielmannsbilder.de>
2018-10-11 19:34:49 +0200

    curriculum seed file added

--- bin/seed/curriculums.js
+++ bin/seed/curriculums.js
@@ -6,16 +6,37 @@ const mongoose = require('mongoose');
 const data = require('../../data/curriculums.js');
 
 const Curriculum = require('../../models/curriculum.js');
+const Unit = require('../../models/unit');
 
 const options = {
   keepAlive: true,
   useNewUrlParser: true,
   reconnectTries: Number.MAX_VALUE
 };
+
+function updateUnits (unit, curriculum, index) {
+  return Unit.findOne({ title: unit })
+    .then((unit) => {
+      if (!unit) {
+        throw new Error('Unknown unit ' + unit);
+      }
+      curriculum.units[index] = unit._id;
+    });
+}
+
+function updateUnitIds (curriculum) {
+  const promisesOfUpdatingOwnerId = curriculum.units.map((unit, index) => updateUnits(unit, curriculum, index));
+  return Promise.all(promisesOfUpdatingOwnerId);
+}
+
 mongoose.connect(process.env.MONGODB_URI, options)
   .then(() => {
     console.log('Connected to Mongo!');
     return Curriculum.remove({});
+  })
+  .then(() => {
+    const promisesOfUpdatingUnitId = data.map((curriculum) => updateUnitIds(curriculum));
+    return Promise.all(promisesOfUpdatingUnitId);
   })
   .then(() => {
     console.log('Empty db');
