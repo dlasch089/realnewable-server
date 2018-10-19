166de85d4972922a71030f6f9a7b0e8b59dcacbc
Dominik Lasch <dl@exutec.de>
2018-10-19 10:58:01 +0200

    adding comments

--- routes/index.js
+++ routes/index.js
@@ -12,23 +12,21 @@ const parseString = require('xml2js').parseString;
 router.get('/', (req, res, next) => {
   request('https://transparency.entsoe.eu/api?securityToken=' + process.env.ENTSOE_KEY + '&documentType=A69&processType=A01&psrType=B16&in_Domain=10YDE-EON------1&periodStart=201810180000&periodEnd=201910180000', (err, response, body) => {
     if (response) {
-      console.log(body);
+      // Convert the xml-string with energy data to a json object
       let xml = body;
       parseString(xml, (error, result) => {
+        // error handler
         if (error) {
           console.log('XML parse error:' + error);
         } else {
-          console.dir(result);
           res.send(result);
         }
       });
-      // res.send(body);
     } else {
       console.log(body.explanation);
-      return console.log('What is this? An error?', err);
+      return console.log('Error message:', err.message);
     }
   });
-  // res.json({ code: 'Hello Ironhackerzzzzzzz XP' });
 });
 
 module.exports = router;
