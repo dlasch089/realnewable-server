cc437e6a43e126954e0df98de4355cab631dcb66
Dominik Lasch <dl@exutec.de>
2018-10-19 10:56:30 +0200

    added: xml to json converter (https://www.npmjs.com/package/xml2js)

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,24 @@ const router = express.Router();
 
 const request = require('request');
 
+const parseString = require('xml2js').parseString;
+
 /* GET home page. */
 /* Request to Entsoe platform */
 router.get('/', (req, res, next) => {
   request('https://transparency.entsoe.eu/api?securityToken=' + process.env.ENTSOE_KEY + '&documentType=A69&processType=A01&psrType=B16&in_Domain=10YDE-EON------1&periodStart=201810180000&periodEnd=201910180000', (err, response, body) => {
     if (response) {
       console.log(body);
-      res.send(body);
+      let xml = body;
+      parseString(xml, (error, result) => {
+        if (error) {
+          console.log('XML parse error:' + error);
+        } else {
+          console.dir(result);
+          res.send(result);
+        }
+      });
+      // res.send(body);
     } else {
       console.log(body.explanation);
       return console.log('What is this? An error?', err);
