06d2768bd1cf6840f1c56580c8c76e25206ac6a3
Dominik Lasch <dl@exutec.de>
2018-10-23 10:36:25 +0200

    extracted time series and period from result

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const parseString = require('xml2js').parseString;
 /* GET home page. */
 /* Request to Entsoe platform */
 router.get('/', (req, res, next) => {
-  request('https://transparency.entsoe.eu/api?securityToken=' + process.env.ENTSOE_KEY + '&documentType=A69&processType=A01&psrType=B16&in_Domain=10YDE-EON------1&periodStart=201810180000&periodEnd=201910180000', (err, response, body) => {
+  request('https://transparency.entsoe.eu/api?securityToken=' + process.env.ENTSOE_KEY + '&documentType=A69&processType=A01&psrType=B16&in_Domain=10YDE-EON------1&periodStart=201810230000&periodEnd=201910230000', (err, response, body) => {
     if (response) {
       // Convert the xml-string with energy data to a json object
       let xml = body;
@@ -19,6 +19,8 @@ router.get('/', (req, res, next) => {
         if (error) {
           console.log('XML parse error:' + error);
         } else {
+          // The time period of the generation is in GL_MarketDocument.TimeSeries[0].Period[0].Point --> Needs to be extracted for each object
+          console.log(result.GL_MarketDocument.TimeSeries[0].Period[0].Point);
           res.send(result);
         }
       });
