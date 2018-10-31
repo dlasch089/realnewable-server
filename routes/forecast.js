5914b9ceb3226f059f0a4bb84a94cf13003fbfc6
Dominik Lasch <dl@exutec.de>
2018-10-31 18:42:39 +0100

    started: realnewable-time-route

--- routes/forecast.js
+++ routes/forecast.js
@@ -7,35 +7,27 @@ const request = require('request');
 
 const parseString = require('xml2js').parseString;
 
-/* GET forecasts. */
-/* Request to Entsoe platform */
-router.get('/realnewable-time', (req, res, next) => {
+/*
+FUNCTION TO FETCH DATA DYNAMICALLY FROM THE API - PROBLEM: ASYNCHRONOUS CODE.
+*/
+
+function fetchData (docType, processType, psrType, in_Domain) {
   request('https://transparency.entsoe.eu/api?securityToken=' + process.env.ENTSOE_KEY + '&documentType=A69&processType=A01&psrType=B16&in_Domain=10YDE-EON------1&periodStart=201810230000&periodEnd=201910230000', (err, response, body) => {
     if (response) {
-      // Convert the xml-string with energy data to a json object
-      let xml = body;
-      parseString(xml, (error, result) => {
-        // error handler
-        if (error) {
-          console.log('XML parse error:' + error);
-        } else {
-          let resultArray = result.GL_MarketDocument.TimeSeries[0].Period[0].Point;
-          // The time period of the generation is in GL_MarketDocument.TimeSeries[0].Period[0].Point --> Needs to be extracted for each object
-          // console.log(result.GL_MarketDocument.TimeSeries[0].Period[0].Point);
-          let newResultArray = resultArray.map(el => {
-            console.log(el);
-            parseInt(el.position);
-            parseInt(el.quantity);
-          });
-          console.log(newResultArray);
-          res.json(result.GL_MarketDocument.TimeSeries[0].Period[0].Point);
-        }
-      });
+      return response;
     } else {
       console.log(body.explanation);
       return console.log('Error message:', err.message);
     }
   });
+}
+
+/* GET forecasts. */
+/* Request to Entsoe platform */
+router.get('/realnewable-time', (req, res, next) => {
+  let result = fetchData();
+  console.log(result);
+  res.json(result);
 });
 
 /*
