0b74fd5c1f4a5eee18f1149b2f7646d195805eec
Dominik Lasch <dl@exutec.de>
2019-02-03 13:17:26 +0100

    commented: cookie-parser

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 // const mongoose = require('mongoose');
@@ -45,7 +45,7 @@ app.use(cors({
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/forecast', forecastRouter);
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-/* Request to Entsoe platform */
 router.get('/', (req, res, next) => {
   res.send('Hello there - this is the official realnewable API. You can find more information on https://github.com/dlasch089/realnewable-server');
 });
